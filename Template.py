cd51c281ade7e320e0dde2afe97a5105a851e51c
Bill Baker <bill@baker88.com>
2020-02-20 11:02:37 -0800

    Initial version

---
+++ Template.py
@@ -0,0 +1,17 @@
+"""
+CTEC 121
+<your name>
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    # code here
+
+main()    
