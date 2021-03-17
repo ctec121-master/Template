124a64901dcc6a798af5369d58cff92a81803a08
Bill Baker <bill@baker88.com>
2021-03-17 09:51:05 -0700

    Correction to file header comment

--- Template.py
+++ Template.py
@@ -3,7 +3,7 @@ CTEC 121
 date: <ex: mm/dd/yyyy>
 <your name>
 Name: <assignment/lab name>
-Description: <assignment/lab description
+Description: <assignment/lab description>
 """
 
 """ IPO template
